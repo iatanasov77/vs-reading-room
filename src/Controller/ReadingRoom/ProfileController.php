72389274fe9968fb501b8e9f93a977ffd4a8ea29
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-01-06 09:32:35 +0000

    Create an Application Profile Page.

--- src/Controller/ReadingRoom/ProfileController.php
+++ src/Controller/ReadingRoom/ProfileController.php
@@ -3,51 +3,31 @@
 use Vankosoft\UsersBundle\Controller\ProfileController as BaseProfileController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Intl\Currencies;
 use Doctrine\Persistence\ManagerRegistry;
 use Sylius\Component\Resource\Factory\FactoryInterface;
-use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Vankosoft\CmsBundle\Component\Uploader\FileUploaderInterface;
 use Vankosoft\UsersBundle\Security\UserManager;
-use Vankosoft\PaymentBundle\Component\Payment\Payment;
+
+use Vankosoft\AgentBundle\Component\VankosoftAgent;
 
 class ProfileController extends BaseProfileController
 {
-	/** @var Payment */
-    private $vsPayment;
-	
-    /** @var RepositoryInterface */
-    private $pricingPlanRepository;
-    
-    /** @var RepositoryInterface */
-    private $pricingPlanCategoryRepository;
-    
-    /** @var RepositoryInterface */
-    private $pricingPlanSubscriptionRepository;
-    
-    public function __construct(
+	public function __construct(
         ManagerRegistry $doctrine,
         string $usersClass,
         UserManager $userManager,
         FactoryInterface $avatarImageFactory,
-        FileUploaderInterface $imageUploader,
-		Payment $vsPayment,
-        RepositoryInterface $pricingPlanRepository,
-        RepositoryInterface $pricingPlanCategoryRepository,
-        RepositoryInterface $pricingPlanSubscriptionRepository
+	    FileUploaderInterface $imageUploader,
+	    VankosoftAgent $vankosoftAgent
     ) {
         parent::__construct(
             $doctrine,
             $usersClass,
             $userManager,
             $avatarImageFactory,
-            $imageUploader
+            $imageUploader,
+            $vankosoftAgent
         );
-        
-		$this->vsPayment                        	= $vsPayment;
-        $this->pricingPlanRepository                = $pricingPlanRepository;
-        $this->pricingPlanCategoryRepository        = $pricingPlanCategoryRepository;
-        $this->pricingPlanSubscriptionRepository    = $pricingPlanSubscriptionRepository;
     }
     
     public function showAction( Request $request ): Response
@@ -58,24 +38,10 @@ class ProfileController extends BaseProfileController
         
         $profileEditForm    = $this->getProfileEditForm();
         $otherForms         = $this->getOtherForms();
-        
-		$activeSubscriptions    = $this->pricingPlanSubscriptionRepository
-                                        ->getActiveSubscriptionsByUser( $this->getUser() );
-        
-        $subscriptionsRoutes    = [];
-        foreach ( $activeSubscriptions as $subscription ) {
-            $subscriptionsRoutes[$subscription->getId()]    = [
-                'createRecurring'   => $this->vsPayment->getPaymentCreateRecurringRoute( $subscription->getGateway() ),
-                'cancelRecurring'   => $this->vsPayment->getPaymentCancelRecurringRoute( $subscription->getGateway() ),
-            ];
-        }
 		
         $params = [
             'profileEditForm'       => $profileEditForm->createView(),
             'changePasswordForm'    => $otherForms['changePasswordForm']->createView(),
-            
-            'subscriptions'         => $this->pricingPlanSubscriptionRepository->getSubscriptionsByUser( $this->getUser() ),
-			'subscriptionsRoutes'   => $subscriptionsRoutes,
         ];
         
         return $this->render( '@VSUsers/Profile/show.html.twig',
